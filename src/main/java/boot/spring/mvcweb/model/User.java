210b73ea40694a49654bb5e95bf015dcc7ba85a9
Ashish Adhikari <ashish.adhikari@infosys.com>
2018-03-12 15:58:02 +0000

    User model added

---
+++ src/main/java/boot/spring/mvcweb/model/User.java
@@ -0,0 +1,17 @@
+/**
+ * @author ashish.adhikari
+ */
+package boot.spring.mvcweb.model;
+
+/**
+ * @author ashish.adhikari
+ *
+ */
+@Data
+public class User {
+	private String userId;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private Integer age;
+}
