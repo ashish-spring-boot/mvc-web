d2e187f42426f5606e5becf18d5ab441e9322f00
Ashish Adhikari <ashish.adhikari@infosys.com>
2018-03-12 13:00:01 +0000

    init

---
+++ src/main/java/boot/spring/mvcweb/controller/GreetingController.java
@@ -0,0 +1,22 @@
+/**
+ * @author ashish.adhikari
+ */
+package boot.spring.mvcweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author ashish.adhikari
+ *
+ */
+@Controller
+public class GreetingController {
+	@GetMapping("/greeting")
+	public String getGreeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+	}
+}
